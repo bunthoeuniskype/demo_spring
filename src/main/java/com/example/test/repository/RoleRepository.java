57a9ceb0f88157728cd21ef2a8ec518b4117b8f6
bunthoeun <bunthoeunmake@gmail.com>
2018-12-10 21:42:14 +0700

    login

---
+++ src/main/java/com/example/test/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.test.model.Role;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
